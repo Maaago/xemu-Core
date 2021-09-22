a256d236743cd3b437756695a166ea72fece0432
Francesco <francesco.magoga@gmail.com>
2021-09-23 00:39:37 +0200

    Added Xcode project

---
+++ xemuGameCore.h
@@ -0,0 +1,15 @@
+//
+//  xemu.h
+//  xemu
+//
+//  Created by Francesco Magoga on 22/09/2021.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <OpenEmuBase/OEGameCore.h>
+
+@class OERingBuffer;
+
+OE_EXPORTED_CLASS
+@interface XemuGameCore : OEGameCore
+@end
---
+++ xemuGameCore.mm
@@ -0,0 +1,7 @@
+//
+//  xemu.mm
+//  xemu
+//
+//  Created by Francesco Magoga on 22/09/2021.
+//
+
